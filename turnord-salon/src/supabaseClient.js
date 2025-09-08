588e1a357476795856d8e33a3bfcf9988310ed27
turnordoficial-hash <turnordoficial@gmail.com>
2025-09-07 21:43:26 -0300

    Update supabaseClient.js
    bien

--- turnord-salon/src/supabaseClient.js
+++ turnord-salon/src/supabaseClient.js
@@ -7,7 +7,7 @@ const supabaseUrl = 'https://nmodhiafyllcudnbkjly.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tb2RoaWFmeWxsY3VkbmJramx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwODk3NzUsImV4cCI6MjA3MDY2NTc3NX0.BAs8YndYaa9S9bd4Y4tKLK-UFQvAxvv1GdjqQuVTyYI'
 
 // Mensaje de advertencia si las credenciales no se han cambiado.
-if (supabaseUrl === 'URL_DE_TU_PROYECTO_SUPABASE' || supabaseAnonKey === 'CLAVE_ANONIMA_PUBLICA_DE_SUPABASE') {
+if (supabaseUrl === 'https://nmodhiafyllcudnbkjly.supabase.co' || supabaseAnonKey === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tb2RoaWFmeWxsY3VkbmJramx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwODk3NzUsImV4cCI6MjA3MDY2NTc3NX0.BAs8YndYaa9S9bd4Y4tKLK-UFQvAxvv1GdjqQuVTyYI') {
   console.warn(
     "ADVERTENCIA: Las credenciales de Supabase no están configuradas. " +
     "Por favor, edita `src/supabaseClient.js` con tus datos para que la aplicación funcione correctamente."
